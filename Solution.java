71505468a6a63969c2da550a9f9bc3cb94450405
Jacob Taylor Cassady <jtcass01@louisville.edu>
2016-06-29 23:57:36 -0400

    Update Solution.java
    Decided to use arrays instead of ArrayLists. Solved problem but some test cases caused a runtime error.  Might be unsolvable for me for now.

--- Solution.java
+++ Solution.java
@@ -4,40 +4,75 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-        int n=0,k=0,luckBalance=0,luck=0,importance=0,importantLosses=0,lowest=0;
-        ArrayList importantValues = new ArrayList();
-        ArrayList lossValues = new ArrayList();
+        int n=0,k=0,luckBalance=0,luck=0,importance=0,importantWins=0,importantValues_i=0;
+        int[] importantValues;
+        int[] winValues;
         
         n = scan.nextInt(); // 1<=n<=100
         k = scan.nextInt(); // 0<=k<=n
+        
+        importantValues = new int[n];
         
         for(int i=0; i<n; i++){
             luck = scan.nextInt(); // 1 <= L <= 10^4
             importance = scan.nextInt(); // 0 <= T <= 1
             if(importance==1){
-                importantValues.add(luck);
+                importantValues[importantValues_i] = luck;
+                importantValues_i++;
             } else {
                 luckBalance+=luck;
             }
         }
         
-        importantLosses = importantValues.size() - k;
-        lowest = importantValues.get(0);
-        for(int importantLosses_i=0;importantLosses_i<importantLosses;importantLosses_i++){
-            for(int importantValues_i=0;importantValues_i<importantValues.size();importantValues_i++){
-                if(importantValues.get(importantValues_i) < lowest){
-                    lowest = importantValues.get(importantValues_i);
-                }
-            }
-            lossValues.add(lowest);
-            importantValues.remove(lowest);
+        importantValues = reduceArray(importantValues,(importantValues_i+1));
+        
+        importantWins = importantValues.length - k;
+        winValues = new int[importantWins];
+        
+        importantValues = sortArray(importantValues);
+        
+        for(int winValues_i=0; winValues_i<winValues.length;winValues_i++){
+            winValues[winValues_i] = importantValues[(importantValues.length-1)-winValues_i];
         }
         
-        for(int importantValues_i=0;importantValues_i<importantValues.size();importantValues_i++){
-            luckBalance+=importantValues.get(importantValues_i);
+        importantValues = reduceArray(importantValues, (importantValues.length-winValues.length));
+        
+        //sum up luck
+        for(int importantLosses_i=0;importantLosses_i<importantValues.length;importantLosses_i++){
+            luckBalance+=importantValues[importantLosses_i];
         }
-        for(int lossValues_i=0;lossValues_i<lossValues.size();lossValues_i++){
-            luckBalance-=lossValues.get(lossvalue_i);
+        for(int winValues_i=0;winValues_i<winValues.length;winValues_i++){
+            luckBalance-=winValues[winValues_i];
         }
+        
+        System.out.println(luckBalance);
+    }
+    
+    public static int[] reduceArray(int[] array, int newSize){
+        int[] resultantArray = new int[newSize];
+        
+        for(int i=0;i<newSize;i++){
+            resultantArray[i] = array[i];
+        }
+        
+        return resultantArray;
+    }
+    
+    public static int[] sortArray(int[] array){
+        int temp=0;
+        boolean change = true;
+        
+        while(change){
+            change = false;
+            for(int i=0; i<(array.length-1);i++){
+                if(array[i] < array[i+1]){
+                    temp = array[i];
+                    array[i] = array[i+1];
+                    array[i+1] = temp;
+                    change = true;
+                }
+            }
+        }
+        return array;
     }
 }
